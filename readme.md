455c67fa2d3d57110ac9325a60993f791ddeb9fd
Islam <islamebotlan@outlook.it>
2023-07-07 12:37:57 +0200

    FIRST COMMIT

--- readme.md
+++ readme.md
@@ -1,8 +1,31 @@
 CREATION PROJET
--composer create-project symfony/website-skeleton blogislem "5.4.*"
+* composer create-project symfony/website-skeleton blogislem "5.4.*"
 
 LIASON PROJET AVEC GIT
--git remote add origin https://github.com/islem9000/blogislem
--git add
--git commit -m "first commit"
--git push --set-upstream origin main
+* git remote add origin https://github.com/islem9000/blogislem
+* git add
+* git commit -m "first commit"
+* git push --set-upstream origin main
+
+CREATION BASE DE DONNEES
+* symfony console doctrine:database:create
+* composer require annotations 
+
+DEMARRAGE SERVER
+* symfony serve
+
+CREATION ENTITE' UTILISATEUR
+* symfony console make:entity
+* symfony.exe console make:migration
+* symfony.exe console doctrine:migrations:migrate
+
+CREATION ENTITE' COMMENT
+* symfony console make:entity
+* symfony.exe console make:migration
+* symfony.exe console doctrine:migrations:migrate
+
+CREATION ENTITE' ARTICLE
+* symfony console make:entity
+* symfony.exe console make:migration
+* symfony.exe console doctrine:migrations:migrate
+
---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Prénom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Pseudo = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrénom(): ?string
+    {
+        return $this->Prénom;
+    }
+
+    public function setPrénom(string $Prénom): static
+    {
+        $this->Prénom = $Prénom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): static
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->Pseudo;
+    }
+
+    public function setPseudo(string $Pseudo): static
+    {
+        $this->Pseudo = $Pseudo;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
